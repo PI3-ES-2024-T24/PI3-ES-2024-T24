81d5d5db2d660ac35e2d1d304c77b097d887751a
Henri costa rios <henricostarios@gmail.com>
2024-02-27 19:32:46 -0300

    Initial commit

---
+++ app/src/main/java/com/puc/pi3_es_2024_t24/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.puc.pi3_es_2024_t24
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PI3-ES-2024-T24"
+include(":app")
+ 
