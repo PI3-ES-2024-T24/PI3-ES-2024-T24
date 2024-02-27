68fefd0f57976d508a5f7e1d8fc6c452f8ba7593
Henri costa rios <henricostarios@gmail.com>
2024-02-27 20:37:42 -0300

    update Sign up

--- app/src/main/java/com/puc/pi3_es_2024_t24/SignUpActivity.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/SignUpActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.puc.pi3_es_2024_t24.databinding.ActivitySignUpBinding
 import com.puc.pi3_es_2024_t24.ui.theme.PI3ES2024T24Theme
 
@@ -24,12 +26,14 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
         binding.btnSignUp.setOnClickListener{
             var email = binding.etEmail.text.toString()
             var password = binding.etPassword.text.toString()
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
                 if(it.isSuccessful){
+                    auth.signOut()
                     Toast.makeText(this, "conta criada", Toast.LENGTH_SHORT).show()
                 }
                 else{
