37b4d6d90b4ff5deb49f69199d3d5067fbbd7a1f
Henri costa rios <henricostarios@gmail.com>
2024-03-06 14:05:21 -0300

    Prototipo home

--- app/src/main/java/com/puc/pi3_es_2024_t24/HomeActivity.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/HomeActivity.kt
@@ -3,7 +3,13 @@ package com.puc.pi3_es_2024_t24
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
@@ -11,23 +17,44 @@ import com.puc.pi3_es_2024_t24.databinding.ActivityHomeBinding
 import com.puc.pi3_es_2024_t24.databinding.ActivitySignInBinding
 import com.puc.pi3_es_2024_t24.databinding.ActivitySignUpBinding
 
+
 class HomeActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-    private lateinit var binding: ActivityHomeBinding
+
+    lateinit var binding : ActivityHomeBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.apply {
+            toggle = ActionBarDrawerToggle(this@HomeActivity, drawerLayout, R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
 
-        auth = Firebase.auth
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        binding.btnLogOut.setOnClickListener{
-            auth.signOut()
-            Toast.makeText(this, "saiu da conta", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-            finish()
+            navView.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.firstItem -> {
+                        Toast.makeText(this@HomeActivity, "First Item Clicked", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.secondtItem -> {
+                        Toast.makeText(this@HomeActivity, "Second Item Clicked", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.thirdItem -> {
+                        Toast.makeText(this@HomeActivity, "third Item Clicked", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                true
+            }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
