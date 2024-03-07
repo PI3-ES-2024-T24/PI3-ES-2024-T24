e099f2c859d94590b1b8b726127868ea048d8106
Henri costa rios <henricostarios@gmail.com>
2024-03-07 12:01:32 -0300

    update drawer
    drawer refeito e funcional

--- app/src/main/java/com/puc/pi3_es_2024_t24/HomeActivity.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/HomeActivity.kt
@@ -6,75 +6,77 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
-import com.puc.pi3_es_2024_t24.databinding.ActivityHomeBinding
+import org.checkerframework.checker.units.qual.A
 
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var bottomNavigateView: BottomNavigationView
     private lateinit var auth: FirebaseAuth
-    private lateinit var binding: ActivityHomeBinding
     private var currentFragment: Fragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
 
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
         auth = Firebase.auth
 
-        setSupportActionBar(binding.toolbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        drawerLayout = binding.drawerLayout
-        bottomNavigateView = binding.bottomNavigation
+        val navigationView =findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
 
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+
 
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        binding.navView.setNavigationItemSelectedListener(this)
-
-        if (savedInstanceState == null) {
-            currentFragment = HomeFragment()
-            replaceFragment(currentFragment!!)
+        if(savedInstanceState == null){
+            replaceFragment(HomeFragment())
+            navigationView.setCheckedItem(R.id.nav_home)
         }
-
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val transation: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transation.replace(R.id.fragment_container, fragment)
+        transation.commit()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_home -> {
-                currentFragment = HomeFragment()
+                replaceFragment(HomeFragment())
             }
             R.id.bottom_home -> {
-                currentFragment = HomeFragment()
+                replaceFragment(HomeFragment())
             }
             R.id.nav_locker -> {
-                currentFragment = LockerFragment()
+                replaceFragment(LockerFragment())
             }
             R.id.bottom_locker -> {
-                currentFragment = LockerFragment()
+                replaceFragment(LockerFragment())
             }
             R.id.nav_settings -> {
-                currentFragment = SettingsFragment()
+                replaceFragment(SettingsFragment())
             }
             R.id.bottom_settings -> {
-                currentFragment = SettingsFragment()
+                replaceFragment(SettingsFragment())
             }
             R.id.nav_about -> {
-                currentFragment = AboutFragment()
+                replaceFragment(AboutFragment())
             }
             R.id.bottom_about -> {
-                currentFragment = AboutFragment()
+                replaceFragment(AboutFragment())
             }
             R.id.nav_logout ->{
                 auth.signOut()
@@ -93,22 +95,16 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.commit()
-    }
+
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
+        super.onBackPressed()
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            if (supportFragmentManager.backStackEntryCount > 0) {
-                supportFragmentManager.popBackStack()
-            } else {
-                super.onBackPressed()
-            }
+
+        }else{
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 }
