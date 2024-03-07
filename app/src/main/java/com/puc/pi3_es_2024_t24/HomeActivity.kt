d50c0514be29b6231fa30a90f06cd4caeb38956a
Henri costa rios <henricostarios@gmail.com>
2024-03-07 18:23:23 -0300

    update floating button new loc

--- app/src/main/java/com/puc/pi3_es_2024_t24/HomeActivity.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/HomeActivity.kt
@@ -1,28 +1,29 @@
 package com.puc.pi3_es_2024_t24
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.puc.pi3_es_2024_t24.databinding.ActivityHomeBinding
-import org.checkerframework.checker.units.qual.A
 
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim)  }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim)  }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim)  }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim)  }
+    private var clicked = false
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityHomeBinding
     private lateinit var fragmentManager: FragmentManager
@@ -33,6 +34,7 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
 
         val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.nav_open, R.string.nav_close)
         binding.drawerLayout.addDrawerListener(toggle)
@@ -52,6 +54,42 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
         fragmentManager = supportFragmentManager
         openFragment(HomeFragment())
+
+        binding.locBtn.setOnClickListener{
+            onAddButtonClicked()
+        }
+        binding.newLocBtn.setOnClickListener{
+            Toast.makeText(this, "Registrar novo locker", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun onAddButtonClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        clicked = !clicked
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if(!this.clicked){
+            binding.newLocBtn.visibility = View.VISIBLE
+            binding.newLocText.visibility = View.VISIBLE
+        }else{
+            binding.newLocBtn.visibility = View.INVISIBLE
+            binding.newLocText.visibility = View.INVISIBLE
+        }
+    }
+    private fun setAnimation(clicked: Boolean) {
+        if(!clicked){
+            binding.newLocBtn.startAnimation(fromBottom)
+            binding.newLocText.startAnimation(fromBottom)
+            binding.locBtn.startAnimation(rotateOpen)
+        }else{
+            binding.newLocBtn.startAnimation(toBottom)
+            binding.newLocText.startAnimation(toBottom)
+            binding.locBtn.startAnimation(rotateClose)
+        }
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
