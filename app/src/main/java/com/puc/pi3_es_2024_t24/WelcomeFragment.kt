e6cfe8437ee4ada076bc1b4376313369b9ffc0f4
Henri costa rios <henricostarios@gmail.com>
2024-03-12 15:48:49 -0300

    update welcome
    add welcome fragment

---
+++ app/src/main/java/com/puc/pi3_es_2024_t24/WelcomeFragment.kt
@@ -0,0 +1,17 @@
+package com.puc.pi3_es_2024_t24
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class WelcomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+    
+}
