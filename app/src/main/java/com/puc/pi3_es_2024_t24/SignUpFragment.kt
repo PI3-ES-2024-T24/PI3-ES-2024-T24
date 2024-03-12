645464829fbb5fae824611e501930bb0a397c1a9
Henri costa rios <henricostarios@gmail.com>
2024-03-12 17:14:34 -0300

    update sign up

--- app/src/main/java/com/puc/pi3_es_2024_t24/SignUpFragment.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/SignUpFragment.kt
@@ -1,16 +1,25 @@
 package com.puc.pi3_es_2024_t24
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.puc.pi3_es_2024_t24.databinding.FragmentSignUpBinding
 import com.puc.pi3_es_2024_t24.databinding.FragmentWelcomeBinding
 
 class SignUpFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding:FragmentSignUpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +29,26 @@ class SignUpFragment : Fragment() {
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
 
         (activity as? AppCompatActivity)?.supportActionBar?.hide()
+        val navController = findNavController()
+        auth = Firebase.auth
+        //caso o botao de id btnSignUp é clicado
+        binding.btnSignUp.setOnClickListener{
+            //formata os dados
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            binding.btnSignUp.setOnClickListener{
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        navController.let {  // Safe navigation using let
+                                it.navigate(R.id.action_signUpFragment_to_signInFragment)
+                        }
 
+                    } else {
+                        Log.e("error: ", it.exception.toString())
+                    }
+                }
+            }
+        }
         return binding.root
     }
 
--- app/src/main/java/com/puc/pi3_es_2024_t24/WelcomeFragment.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/WelcomeFragment.kt
@@ -1,11 +1,14 @@
 package com.puc.pi3_es_2024_t24
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.puc.pi3_es_2024_t24.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -20,6 +23,13 @@ class WelcomeFragment : Fragment() {
 
         (activity as? AppCompatActivity)?.supportActionBar?.hide()
 
+        val navController = findNavController()
+
+        navController.let {  // Safe navigation using let
+            Handler(Looper.getMainLooper()).postDelayed({
+                it.navigate(R.id.action_welcomeFragment_to_signUpFragment)
+            }, 2000)
+        }
         return binding.root
 
     }
