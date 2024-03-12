924f1e117cd2c1a73af9550254e20a204ac84511
Henri costa rios <henricostarios@gmail.com>
2024-03-12 16:18:20 -0300

    update welcome and main

--- app/src/main/java/com/puc/pi3_es_2024_t24/MainActivity.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/MainActivity.kt
@@ -3,10 +3,18 @@ package com.puc.pi3_es_2024_t24
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController= findNavController(R.id.navHostFragmentContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
