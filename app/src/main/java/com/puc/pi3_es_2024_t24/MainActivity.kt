de6c6e5859135ec8dec3f6651c7124e71b5e46a9
Henri costa rios <henricostarios@gmail.com>
2024-03-12 16:11:42 -0300

    update nav_graph

--- app/src/main/java/com/puc/pi3_es_2024_t24/MainActivity.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/MainActivity.kt
@@ -2,6 +2,7 @@ package com.puc.pi3_es_2024_t24
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
