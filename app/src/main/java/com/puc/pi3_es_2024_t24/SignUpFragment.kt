4e2f5765b73fb72170916b1a9f9e827e36be0715
Henri costa rios <henricostarios@gmail.com>
2024-03-12 17:25:16 -0300

    update nav_graph and sign up

--- app/src/main/java/com/puc/pi3_es_2024_t24/SignUpFragment.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/SignUpFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -36,19 +37,19 @@ class SignUpFragment : Fragment() {
             //formata os dados
             val email = binding.etEmail.text.toString().trim()
             val password = binding.etPassword.text.toString().trim()
-            binding.btnSignUp.setOnClickListener{
                 auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
                     if (it.isSuccessful) {
-                        navController.let {  // Safe navigation using let
-                                it.navigate(R.id.action_signUpFragment_to_signInFragment)
-                        }
+                        navController.navigate(R.id.action_signUpFragment_to_signInFragment)
 
                     } else {
                         Log.e("error: ", it.exception.toString())
                     }
                 }
-            }
         }
+        binding.btnLogin.setOnClickListener{
+            it.findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
         return binding.root
     }
 
