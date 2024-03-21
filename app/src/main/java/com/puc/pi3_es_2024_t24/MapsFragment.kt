7d3c6ebde32e2e52b492af8baa980abd96400bc5
Henri costa rios <henricostarios@gmail.com>
2024-03-21 11:30:36 -0300

    update maps
    Co-Authored-By: Kauan Barbosa <77552839+kauanbarbosa@users.noreply.github.com>

--- app/src/main/java/com/puc/pi3_es_2024_t24/MapsFragment.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/MapsFragment.kt
@@ -17,14 +17,29 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.puc.pi3_es_2024_t24.databinding.FragmentMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLngBounds
 
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
+    private val location1 = MarkerData(
+        "LOCAL 1",
+        LatLng(-22.8345916,-47.0540574),
+        "Av. Profa. Ana Maria Silvestre Adade, 255-395 - Parque das Universidades, Campinas - SP",
+        4.9f,
+        "Em frente a PUCCAMPINAS"
+    )
+
+    private val location2 = MarkerData(
+        "LOCAL 2",
+        LatLng(-22.8440713,-47.0531428),
+        "Rua A Strazzacappa, 470 - Vila Embare, Valinhos - SP",
+        4.9f,
+        "Em frente ao Campinas Hall"
+    )
 
     private lateinit var map: GoogleMap
     private lateinit var binding: FragmentMapsBinding
-    private val _markers = MutableLiveData<List<MarkerData>>()
-    val markers: LiveData<List<MarkerData>> = _markers
+    private val locations = arrayListOf<MarkerData>(location1, location2)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,14 +52,26 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
         Log.d(TAG, "Criado")
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        mapFragment.getMapAsync { googleMap ->
+            googleMap.setInfoWindowAdapter(MarkerInfoAdapter(requireContext()))
+            addMarkers(googleMap)
+
+            googleMap.setOnMapLoadedCallback {
+                val bounds = LatLngBounds.builder()
+
+                locations.forEach {
+                    bounds.include(it.latLng)
+                }
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 100))
+            }
+        }
         Log.d(TAG, "sincronizado")
 
     }
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         Log.d(TAG, "Mapa pronto")
-        addCustomMarkers(listOf(MarkerData( "LOCAL 1",LatLng(-22.8345916,-47.0540574), "Av. Profa. Ana Maria Silvestre Adade, 255-395 - Parque das Universidades, Campinas - SP", 4.9f,"Em frente a PUCCAMPINAS")))
         Log.d(TAG, "Marcador posicionado")
         val puc = LatLng(-22.83400, -47.05276)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(puc, 15f))
@@ -53,17 +80,18 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     }
 
 
-    private fun addCustomMarkers(markers: List<MarkerData>) {
-        _markers.value = markers.map { marker ->
-            val markerOptions = MarkerOptions()
-                .title(marker.name)
-                .snippet(marker.address)
-                .position(marker.latLng)
-                .icon(
-                    BitmapHelper.vectorToBitmap(requireContext(), R.drawable.lock_icon, ContextCompat.getColor(requireContext(), androidx.appcompat.R.color.material_blue_grey_800))
-                )
-            map.addMarker(markerOptions)
-            marker
+    private fun addMarkers(googleMap: GoogleMap) {
+        locations.forEach { location ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(location.name)
+                    .snippet(location.address)
+                    .position(location.latLng)
+                    .icon(
+                        BitmapHelper.vectorToBitmap(requireContext(), R.drawable.lock_icon, ContextCompat.getColor(requireContext(), androidx.appcompat.R.color.material_blue_grey_800))
+                    )
+            )
+            marker?.tag = location
         }
     }
 }
