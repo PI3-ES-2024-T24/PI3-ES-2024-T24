5bbea466486bc1918a97dda6056455888d8f22a6
Henri costa rios <henricostarios@gmail.com>
2024-03-21 13:49:42 -0300

    Update ForgotPasswordFragment.kt
    add basic binding

--- app/src/main/java/com/puc/pi3_es_2024_t24/ForgotPasswordFragment.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/ForgotPasswordFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.puc.pi3_es_2024_t24.databinding.FragmentForgotPasswordBinding
 
 class ForgotPasswordFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: FragmentForgotPasswordBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+        binding = FragmentForgotPasswordBinding.inflate(inflater, container, false)
+
+        (activity as? AppCompatActivity)?.supportActionBar?.hide()
+        val navController = findNavController()
+        auth = Firebase.auth
+
+        return binding.root
     }
 
 
