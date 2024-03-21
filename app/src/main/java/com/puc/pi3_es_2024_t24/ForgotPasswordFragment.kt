0b430467da0217ef78bfd42d03a87942700494be
Henri costa rios <henricostarios@gmail.com>
2024-03-21 13:42:27 -0300

    update forgotPassword
    add esqueceu senha fragmento

---
+++ app/src/main/java/com/puc/pi3_es_2024_t24/ForgotPasswordFragment.kt
@@ -0,0 +1,21 @@
+package com.puc.pi3_es_2024_t24
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.puc.pi3_es_2024_t24.databinding.FragmentForgotPasswordBinding
+
+class ForgotPasswordFragment : Fragment() {
+    private lateinit var binding: FragmentForgotPasswordBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+
+
+}
