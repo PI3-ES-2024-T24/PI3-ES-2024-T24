afb9b30718c791cc1d564d5f5643266fdbf96806
Henri costa rios <henricostarios@gmail.com>
2024-04-30 13:49:07 -0300

    update camera permissions
    added request camera permissions

--- app/src/main/java/com/puc/pi3_es_2024_t24/CameraFragment.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/CameraFragment.kt
@@ -1,24 +1,54 @@
 package com.puc.pi3_es_2024_t24
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.Camera
 import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.SupportMapFragment
+import com.puc.pi3_es_2024_t24.databinding.FragmentCameraBinding
 
 class CameraFragment : Fragment() {
     private lateinit var imageCapture: ImageCapture
     private lateinit var camera: Camera
+    private lateinit var binding: FragmentCameraBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+    ): View {
+        binding = FragmentCameraBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkCameraPermissions()
+
+    }
+
+    private fun checkCameraPermissions() {
+        //cria as instruçôes de permissão para o aplicativo da localização precisa ou aproximada
+        val cameraPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                Toast.makeText(requireContext(), "Camera permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Camera permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //lança as instruções de permissão
+        cameraPermissionRequest.launch(Manifest.permission.CAMERA)
+    }
 
 }
--- app/src/main/java/com/puc/pi3_es_2024_t24/main/SignInFragment.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/main/SignInFragment.kt
@@ -66,6 +66,9 @@ class SignInFragment : Fragment() {
         binding.forgotPassword.setOnClickListener {
             it.findNavController().navigate(R.id.action_signInFragment_to_forgotPasswordFragment)
         }
+        binding.testCamera.setOnClickListener {
+            it.findNavController().navigate(R.id.action_signInFragment_to_cameraFragment)
+        }
         return binding.root
     }
     private fun validate(): Boolean{
