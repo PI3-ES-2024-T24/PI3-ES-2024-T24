720ca29971bc261cbb1730bfd7cdc15c5f7ce4c9
Henri costa rios <henricostarios@gmail.com>
2024-04-30 14:13:40 -0300

    camera image working
    camera is working(taking picture is not implemented yet)

--- app/src/main/java/com/puc/pi3_es_2024_t24/CameraFragment.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/CameraFragment.kt
@@ -1,8 +1,10 @@
 package com.puc.pi3_es_2024_t24
 
 import android.Manifest
+import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,16 +12,20 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
-import androidx.core.app.ActivityCompat
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
-import com.google.android.gms.maps.SupportMapFragment
 import com.puc.pi3_es_2024_t24.databinding.FragmentCameraBinding
 
 class CameraFragment : Fragment() {
     private lateinit var imageCapture: ImageCapture
     private lateinit var camera: Camera
     private lateinit var binding: FragmentCameraBinding
+    private lateinit var cameraProvider:ProcessCameraProvider
+    private lateinit var cameraSelector: CameraSelector
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,12 +42,12 @@ class CameraFragment : Fragment() {
     }
 
     private fun checkCameraPermissions() {
-        //cria as instruçôes de permissão para o aplicativo da localização precisa ou aproximada
         val cameraPermissionRequest = registerForActivityResult(
             ActivityResultContracts.RequestPermission()
         ) { isGranted ->
             if (isGranted) {
                 Toast.makeText(requireContext(), "Camera permission granted", Toast.LENGTH_SHORT).show()
+                startCamera()
             } else {
                 Toast.makeText(requireContext(), "Camera permission denied", Toast.LENGTH_SHORT).show()
             }
@@ -50,5 +56,27 @@ class CameraFragment : Fragment() {
         //lança as instruções de permissão
         cameraPermissionRequest.launch(Manifest.permission.CAMERA)
     }
+private fun startCamera(){
+    val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+    cameraProviderFuture.addListener({
+        cameraProvider = cameraProviderFuture.get()
+        val preview = Preview.Builder()
+            .build()
+            .also {
+                it.setSurfaceProvider(binding.previewView.surfaceProvider)
+            }
+        cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        try {
+            cameraProvider.unbindAll()
+
+            cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview)
+
+        } catch(exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+
+    }, ContextCompat.getMainExecutor(requireContext()))
+}
 
 }
