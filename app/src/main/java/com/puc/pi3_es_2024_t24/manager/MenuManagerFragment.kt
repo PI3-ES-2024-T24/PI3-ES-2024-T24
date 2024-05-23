1dda718b7e532eb8d44409c2bb1149f361767c55
henricr <henricostarios@gmail.com>
2024-05-23 17:55:06 -0300

    update sign out

--- app/src/main/java/com/puc/pi3_es_2024_t24/manager/MenuManagerFragment.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/manager/MenuManagerFragment.kt
@@ -1,25 +1,31 @@
 package com.puc.pi3_es_2024_t24.manager
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.puc.pi3_es_2024_t24.R
 import com.puc.pi3_es_2024_t24.databinding.FragmentLocationSuccessBinding
 import com.puc.pi3_es_2024_t24.databinding.FragmentMenuManagerBinding
+import com.puc.pi3_es_2024_t24.main.MainActivity
 
 class MenuManagerFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
 
     private lateinit var binding: FragmentMenuManagerBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentMenuManagerBinding.inflate(inflater, container, false)
 
+        auth = Firebase.auth
+        binding = FragmentMenuManagerBinding.inflate(inflater, container, false)
 
         binding.btnReleaseLocker.setOnClickListener {
             findNavController().navigate(R.id.action_menuManagerFragment_to_cameraFragment)
@@ -29,6 +35,10 @@ class MenuManagerFragment : Fragment() {
         }
 
         binding.btnLogOutManager.setOnClickListener {
+            val intent = Intent(requireContext(),MainActivity::class.java)
+            startActivity(intent)
+            auth.signOut()
+            requireActivity().finish()
         }
         return binding.root
     }
