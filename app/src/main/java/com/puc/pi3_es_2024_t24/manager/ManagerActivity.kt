1ab14ed1b24cc03b4965975d07bffa11ed7945ad
Henri costa rios <henricostarios@gmail.com>
2024-05-24 20:43:23 -0300

    update fix

--- app/src/main/java/com/puc/pi3_es_2024_t24/manager/ManagerActivity.kt
+++ app/src/main/java/com/puc/pi3_es_2024_t24/manager/ManagerActivity.kt
@@ -2,17 +2,13 @@ package com.puc.pi3_es_2024_t24.manager
 
 import android.content.Intent
 import android.nfc.NfcAdapter
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.puc.pi3_es_2024_t24.R
-import com.puc.pi3_es_2024_t24.databinding.FragmentMenuManagerBinding
 import com.puc.pi3_es_2024_t24.models.NfcTag
 
 class ManagerActivity : AppCompatActivity() {
